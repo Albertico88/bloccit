f8f0385c08fc16ca050d969d9ca2e33e1e12e553
Alberto <alberto.grau.margarida@gmail.com>
2015-09-14 14:43:04 -0400

    assignment 26

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
+  get 'welcome/faq'
+
   root to: 'welcome#index'
   # The root method allows us to declare the default page your app loads when we type the home page URL.
 end
