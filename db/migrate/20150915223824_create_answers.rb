040e3736a07e217780fa1355936fca11291253dc
Alberto <alberto.grau.margarida@gmail.com>
2015-09-16 15:09:12 -0400

    fixed to be lowercased

--- db/migrate/20150915223824_create_answers.rb
+++ db/migrate/20150915223824_create_answers.rb
@@ -2,10 +2,11 @@ class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
       t.text :body
-      t.references :Question, index: true
+      #changed to lowercase
+      t.references :question, index: true
 
       t.timestamps null: false
     end
-    add_foreign_key :answers, :Questions
+    add_foreign_key :answers, :questions
   end
 end
