4abb03722211a85429e50f95302afe127c53e32b
Alberto <alberto.grau.margarida@gmail.com>
2015-10-15 15:43:09 -0400

    Checkpoint 38 completed, Validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,9 @@ class Post < ActiveRecord::Base
   belongs_to :topic
   has_many :comments, dependent: :destroy
   # the has_many method allows a post to have many comments related to it.
+
+  validates :title, length: {minimum: 5}, presence: true
+  validates :body, length: {minimum: 20}, presence: true
+  validates :topic, presence: true
+  # validates the presence of title, body and topic with respective lengths
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -11,7 +11,14 @@ RSpec.describe Post, type: :model do
 
   # it { should belong_to(:topic)}
   # it { should have_many(:comments)}
-  # shoulda-matchers not working properly
+
+  # --> shoulda-matchers NOT WORKING PROPERLY <--
+
+  # it { should validate_presence_of(:title) }
+  # it { should validate_presence_of(:body) }
+  # it { should validate_presence_of(:topic) }
+  # it { should validate_length_of(:title).is_at_least(5) }
+  # it { should validate_length_of(:body).is_at_least(20) }
 
   context "attributes" do
     it "should respond to title" do
