031a60cdfe9104ef461627b73fc6192d1de3a3bd
Alberto <alberto.grau.margarida@gmail.com>
2015-10-28 16:19:25 -0400

    assignment 37, validations completed

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,10 @@ Rails.application.routes.draw do
 
   get 'faq' => 'welcome#faq'
 
+  post 'users/confirm' => 'users#confirm'
+
   root to: 'welcome#index'
   # The root method allows us to declare the default page your app loads when we type the home page URL.
 end
 
-# $ rake routes     Shows all routes
+# $ rake routes       Shows all routes
