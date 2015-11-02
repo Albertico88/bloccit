0c15bdf3e008f53f614ce6ac0734254ad009b537
Alberto <alberto.grau.margarida@gmail.com>
2015-11-02 15:49:40 -0500

    Assignment 28 completed, Sign in Sessions

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,6 @@
 module UsersHelper
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "<img src='http://gravatar.com/avatar/#{gravatar_id}.png?s=32' />".html_safe
+  end
 end
