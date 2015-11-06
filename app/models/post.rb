5a69ac83638a4c8310f5eca986c7000c71a2b9e6
Alberto <alberto.grau.margarida@gmail.com>
2015-11-06 15:39:38 -0500

    Assignment 39, adding Scopes to Post class, completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,9 +7,12 @@ class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
   # the has_many method allows a post to have many comments related to it.
 
-#This will display posts in order by their created at date.
+# This will display posts in order by their created at date.
   default_scope { order('created_at DESC') }
+# Scope will order results by title
   scope :ordered_by_title, -> { order('title DESC') }
+# Scope will order results by time of creation
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC')}
 
   validates :title, length: {minimum: 5}, presence: true
   validates :body, length: {minimum: 20}, presence: true
