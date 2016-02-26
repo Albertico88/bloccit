06d60fa76405f41e06086a8efa52fcdd1d751a10
Alberto <alberto.grau.margarida@gmail.com>
2016-02-26 16:34:00 -0500

    Checkpoint 45 Public Profiles completed

--- spec/controllers/favorites_controller_spec.rb
+++ spec/controllers/favorites_controller_spec.rb
@@ -36,7 +36,10 @@ RSpec.describe FavoritesController, type: :controller do
         end
 
         it 'creates a favorite for the current user and specified post' do
-          expect(my_user.favorites.find_by_post_id).not_to be_nil
+          expect(my_user.favorites.find_by_post_id(my_post.id)).to be_nil
+
+       post :create, { post_id: my_post.id }
+       expect(my_user.favorites.find_by_post_id(my_post.id)).not_to be_nil
         end
       end
 
